05e974c5bda5eee834cfe6fc200718b37662ce97
Luis <luisfiis06@gmail.com>
2019-06-01 17:21:20 -0500

    arquetipos proyectos

---
+++ movie-info-service/src/main/java/pe/com/lgutierrez/moviecinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lgutierrez.moviecinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
---
+++ rating-data-service/src/main/java/pe/com/lgutierrez/ratingdataservice/RatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lgutierrez.ratingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingDataServiceApplication.class, args);
+	}
+
+}
